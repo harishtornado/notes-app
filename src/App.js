f90fb2cc88086de5c63e3f36c5ec09392f8ae886
harish <77318230+harishtornado@users.noreply.github.com>
2023-11-12 21:42:40 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,102 +1,153 @@
 import { useEffect, useRef, useState } from "react";
-import send_btn from './assets/send.svg';
+import send_btn from "./assets/send.svg";
+import back_btn from "./assets/back.svg";
+import Modal from "./components/Modal";
+import Note from "./components/Note";
+import note_bg from "./assets/notes-background.svg";
 
 const App = () => {
-  const inputref = useRef()
-  const [groups,setGroups] = useState([
-    {
-      name : "sample",
-      color: "#f00",
-      notes: [
-        {
-          time: "",
-          content: "Lorem5050505005"
-        },
-        {
-          time: "",
-          content: "Lorem5050505005"
-        },
-        {
-          time: "",
-          content: "Lorem5050505005"
-        },
-      ]
-    },
-    {
-      name : "sample1",
-      color: "#f00",
-      notes: [
-        {
-          time: "",
-          content: "Lorem5050505005"
-        },
-        {
-          time: "",
-          content: "Lorem5050505005"
-        },
-        {
-          time: "",
-          content: "Lorem5050505005"
-        },
-      ]
-    },
-  ])
-  const [currGroup,setCurrGroup] = useState("");
+  const inputref = useRef();
+
+  const [modal, setModal] = useState(false);
+
+  const [groups, setGroups] = useState([]);
 
   useEffect(() => {
-    if(currGroup !== ""){
-      const right = document.querySelector('.right_section')
-      const left = document.querySelector('.left_section')
-      left.style.display = "none"
-      right.style.display = "block"
+    if (localStorage.getItem("groups")) {
+      if (groups.length > 0) {
+        localStorage.setItem("groups", JSON.stringify(groups));
+      }
+      const group = localStorage.getItem("groups");
+      setGroups(JSON.parse(group));
+    } else {
+      console.log(groups);
+      localStorage.setItem("groups", JSON.stringify(groups));
     }
-  },[currGroup])
+  }, [groups]);
+
+  const [currGroup, setCurrGroup] = useState(null);
 
   const uploadNote = () => {
-    groups?.map((group) => {
-      if(group.name === groups[currGroup].name){
-        const newNote = [...group.notes, {time: "",content: inputref.current.value}]
-        console.log(group)
-      }
-    })
-  }
+    const content = inputref.current.value;
+    if (content === "") {
+      return;
+    }
+    setGroups((prevGroups) => {
+      return prevGroups.map((group) => {
+        if (group.name === groups[currGroup].name) {
+          const updatedGroup = { ...group };
+          const newNote = {
+            time: new Date().toLocaleString(),
+            content: content,
+          };
+          updatedGroup.notes = [...group.notes, newNote];
+          return updatedGroup;
+        } else {
+          return group;
+        }
+      });
+    });
+    inputref.current.value = "";
+  };
+
+  useEffect(() => {
+    const root = document.querySelector("#root");
+    if (currGroup === null) {
+      root.style.setProperty("--display1", "block");
+      root.style.setProperty("--display2", "none");
+    } else {
+      root.style.setProperty("--display1", "none");
+      root.style.setProperty("--display2", "block");
+    }
+  }, [currGroup]);
 
   return (
     <div className="App container">
       <div className="left_section">
         <h1 className="header">Pocket Notes</h1>
-        <div className="create_btn" onClick={() => console.log("clicked")}>+ Create Notes Group</div>
+        <div className="create_btn" onClick={() => setModal(true)}>
+          + Create Notes Group
+        </div>
         <div className="group_list">
-          {
-            groups.map((group,index) => (
-              <div className={`group ${groups[currGroup]?.name === group.name ? "active" : null}`} key={index} onClick={() => {setCurrGroup(index)}}>
-                <div className="avatar">{group.name.slice(0,2)}</div>
-                <div className="group_name">{group.name}</div>
-              </div>
-            ))
-          }
+          {groups?.map((group, index) => (
+            <Group
+              groups={groups}
+              currGroup={currGroup}
+              key={index}
+              group={group}
+              setCurrGroup={setCurrGroup}
+              index={index}
+            />
+          ))}
         </div>
       </div>
       <div className="right_section">
-        <div className="notes_list">
-          {
-            groups[currGroup]?.notes?.map((note,index) => (
-              <div className="note" key={index}>
-                <div className="time">{note.time}</div>
-                <div className="note_content">{note.content}</div>
+        {currGroup !== null ? (
+          <>
+            <div className="group_header">
+              <div className="back_btn" onClick={() => setCurrGroup(null)}>
+                <img src={back_btn} alt="" />
               </div>
-            ))
-          }
-        </div>
-        <div className="input_box">
-          <textarea ref={inputref} placeholder="Enter your text here..........."></textarea>
-          <div className="send_btn" onClick={() => uploadNote()}>
-            <img src={send_btn} />
+              <div
+                className="group_avatar"
+                style={{ background: groups[currGroup]?.color }}
+              >
+                {groups[currGroup]?.name?.slice(0, 2)}
+              </div>
+              <div className="group_heading">{groups[currGroup]?.name}</div>
+            </div>
+            <div className="notes_list">
+              {groups[currGroup]?.notes?.map((note, index) => (
+                <Note key={index} note={note} />
+              ))}
+            </div>
+            <div className="input_box">
+              <textarea
+                ref={inputref}
+                placeholder="Enter your text here..........."
+              ></textarea>
+              <div className="send_btn" onClick={() => uploadNote()}>
+                <img src={send_btn} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="blank_templete">
+            <img src={note_bg} alt="note_bg" />
+            <h1>Pocket Notes</h1>
+            <p>
+              Send and receive messages without keeping your phone online. Use
+              Pocket Notes on up to 4 linked devices and 1 mobile phone
+            </p>
           </div>
-        </div>
+        )}
       </div>
+      {modal && (
+        <Modal
+          setModal={setModal}
+          modal={modal}
+          setGroups={setGroups}
+          groups={groups}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
+const Group = ({ currGroup, group, groups, setCurrGroup, index }) => (
+  <div
+    className={`group ${
+      groups[currGroup]?.name === group.name ? "active" : null
+    }`}
+    onClick={() => {
+      setCurrGroup(index);
+    }}
+  >
+    <div className="avatar" style={{ background: group.color }}>
+      {group.name.slice(0, 2)}
+    </div>
+    <div className="group_name">{group.name}</div>
+  </div>
+);
